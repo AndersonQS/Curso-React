44631cde9fe6cf867a4f2f2d1cd6dec4ae8752fb
Anderson <andersonaracati10@gmail.com>
2021-12-13 19:09:48 -0300

    React Fragment

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
+import Fragmento from './components/basicos/Fragmento'
+
+
 ReactDOM.render(
   <div id = "app"> 
     <Primeiro></Primeiro>
@@ -10,6 +13,8 @@ ReactDOM.render(
     titulo = "segundo componente"
     aluno = "Anderson"
     nota = {5.4}/>
+    <Fragmento></Fragmento>
+
   
   </div>,
   document.getElementById('root'));
