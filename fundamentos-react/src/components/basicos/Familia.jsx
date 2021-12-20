19d3c64aa25380ec5832b30a81d7cb0904485ed1
Anderson <andersonaracati10@gmail.com>
2021-12-20 19:43:35 -0300

    Componente filho

---
+++ fundamentos-react/src/components/basicos/Familia.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import FamiliaMembro from './FamiliaMembro';
+
+// import { Container } from './styles';
+
+export default props => {
+  return 
+  <div>
+    <FamiliaMembro></FamiliaMembro>
+    <FamiliaMembro></FamiliaMembro>
+    <FamiliaMembro></FamiliaMembro>
+  </div>;
+}
+
